d3c3bb48d8b747c7274cd1d11a3c28a18879d2b4
Leo <ronald.mauritsson@gmail.com>
2023-07-02 19:00:06 +0300

    Addd Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logoWhite from "./assets/images/logo-white.png";
+import underConstruction from "./assets/images/under-construction.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img src={logoWhite} className="mx-auto  h-[40vmin]" alt="logo" />
+        {/* <img
+          src={underConstruction}
+          className="object-cover  h-[100%] w-[100%]"
+          alt="logo"
+        /> */}
+        <p>Website currently under construction.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/NEXTCHAPTERTEC"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit our socials
         </a>
       </header>
     </div>
