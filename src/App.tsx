f2c3210341a86f448ee5195ce66e572647da26d8
Leo <ronald.mauritsson@gmail.com>
2023-07-02 19:03:17 +0300

    Removed unused assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import logoWhite from "./assets/images/logo-white.png";
-import underConstruction from "./assets/images/under-construction.jpg";
 import "./App.css";
 
 function App() {
@@ -8,11 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logoWhite} className="mx-auto  h-[40vmin]" alt="logo" />
-        {/* <img
-          src={underConstruction}
-          className="object-cover  h-[100%] w-[100%]"
-          alt="logo"
-        /> */}
         <p>Website currently under construction.</p>
         <a
           className="App-link"
